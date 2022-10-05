6e633c6d748daacc3a60492bdbb2c79880c5ca2e
frankot12 <114936414+frankot12@users.noreply.github.com>
2022-10-04 21:37:42 -0500

    Update Raices cuadraticas.cpp

--- Raices cuadraticas.cpp
+++ Raices cuadraticas.cpp
@@ -1,91 +1,24 @@
 /******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+                       REALIZADO POR : Frank Rios 6490 - Kevin Logroño 6654
 
 *******************************************************************************/
 
-#include<stdio.h>
-#include<math.h>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-
-int main(int argc, char** args){
-    bool esEntero(string);
-	string linea;
-	int numero;
-	bool repite = true;
- float a, b, c, disc, x1, x2, xi, xr;
- char letra;
- 
- printf("\n\t\tRAICES DE UN POLINOMIO DE SEGUNDO GRADO");
- printf("\n\t\t_________________________________________\n\n\n");
- 	
- 	
- 	
- //funcion que valida que solo sea numeros
- 	do {
-		
-		cout << "\nIngrese un valor entero: ";
-		
-		getline(cin, linea);
-		
-		if (esEntero(linea)) {
-			repite = false;
-		} else {
-			cout << "No has ingresado un valor entero. Intentalo nuevamente" << endl;
-		}
-	} while (repite);
- 
-
- do {
- printf("\t\tIngrese un valor dentro del rango 1 -1000 -->");
-cin>> a;
- 
-     
- } while(a>1000);
-
- while(a==0){
- printf("\t\tEl valor de a no puede ser 0 ingrese el valor de nuevo -->");
- scanf("%f", &a);
- }
- printf("\t\t\tIngrese el valor de b --> ");
- scanf("%f", &b);
-printf("\t\t\tIngrese el valor de c --> ");
- scanf("%f", &c);
- disc=pow(b, 2.0)-4*a*c;
- if(disc>0.0){
- printf("\t\t\tLas dos raices son reales");
- x1=((-b+sqrt(disc))/(2.0*a));
- x2=((-b-sqrt(disc))/(2.0*a));
- printf("\n\t\t\tx1=%.2f x2=%.2f", x1, x2);
- }
- else{
- if(disc==0.0){
- x1=(-b)/(2.0*a);
- printf("\n\t\t\tLa ecuacion solo tiene una raiz %.2f", x1);
- }
- else{
- xr=(-b/(2.0*a));
- xi=(sqrt(-disc)/(2.0*a));
- printf("\n\t\tLa raiz real es %.2f y la imaginaria es %.2f", xr, xi);
- }
- }
- printf("\n\n\t\t\t");
-}
-
-
 
 bool esEntero(string linea) {
 	bool esEntero = true;
 	int longitud = linea.size();
 	
+	
 	if (longitud == 0) {
 		esEntero = false;
 	} else if (longitud == 1 && !isdigit(linea[0])) {
 		esEntero = false;
+
 	} else {
 		int indice = 0;
 		if (linea[0] == '+' || linea[0] == '-') {
@@ -102,7 +35,92 @@ bool esEntero(string linea) {
 			indice++;
 		}
 	}
-	
-	
 	return esEntero;	
+}
+
+int main(int argc, char** args){
+    float a, b, c, disc, x1, x2, xi, xr;
+    string val11,val22,val33;
+    bool val1 = true;
+    bool val2 = true;
+    bool val3 = true;
+    printf("\n\t\tSolucion de una ecuacion de segundo grado");
+    printf("\n\t\t_______________\n\n\n");
+
+    do {
+	    cout << "\nIngrese el valor de a: ";
+	    getline(cin, val11);
+		
+		//mayor a 1000
+		if (atoi(val11.c_str()) > 1000) {
+			cout << "No has ingresado un valor menor que 1000, ingresa de nuevo" << endl;
+		}
+		
+		if (atoi(val11.c_str()) == 0) {
+			cout << "Esta variable no puede ser igual a 0, ingresa de nuevo" << endl;
+		}
+		
+	    if (esEntero(val11)) {
+			val1 = false;
+			a = atoi(val11.c_str());
+	    } else {
+			cout << "No has ingresado un valor numerico, ingresa de nuevo" << endl;
+	    }
+	} while ((atoi(val11.c_str())==0)||(atoi(val11.c_str())>=1000));
+	
+
+    do {
+	    cout << "\nIngrese un valor de b: ";
+	    getline(cin, val22);
+		
+		if (atoi(val22.c_str()) > 1000) {
+			cout << "No has ingresado un valor menor que 1000, ingresa de nuevo" << endl;
+		}
+		
+	    if (esEntero(val22)) {
+			val2 = false;
+			b = atoi(val22.c_str());
+    	} else {
+			cout << "No has ingresado un valor correcto, ingresa de nuevo" << endl;
+	    }
+	} while ((val2) || (atoi(val22.c_str())>=1000));
+	
+	
+	
+	do {
+	    cout << "\nIngrese un valor de c: ";
+	    getline(cin, val33);
+		
+		if (atoi(val33.c_str()) > 1000) {
+			cout << "No has ingresado un valor menor que 1000, ingresa de nuevo" << endl;
+		}
+		
+	    if (esEntero(val33)) {
+			val3 = false;
+	    } else {
+			cout << "NNo has ingresado un valor correcto, ingresa de nuevo" << endl;
+	    }
+	} while ((val3) || (atoi(val33.c_str())>=1000));
+	
+	c= atoi(val33.c_str());
+	
+    disc=pow(b, 2.0)-4*a*c;
+    if(disc>0.0){
+        printf("\t\t\tLas dos raices son reales");
+        x1=((-b+sqrt(disc))/(2.0*a));
+        x2=((-b-sqrt(disc))/(2.0*a));
+        printf("\n\t\t\tx1=%.2f x2=%.2f", x1, x2);
+    }
+ else{
+    if(disc==0.0){
+        x1=(-b)/(2.0*a);
+        printf("\n\t\t\tLa ecuacion solo tiene una raiz %.2f", x1);
+    }
+    else{
+        xr=(-b/(2.0*a));
+        xi=(sqrt(-disc)/(2.0*a));
+        printf("\n\t\tLa raiz real es %.2f y la imaginaria es %.2f", xr, xi);
+    }
+    }
+ printf("\n\n\t\t\t");
 }
